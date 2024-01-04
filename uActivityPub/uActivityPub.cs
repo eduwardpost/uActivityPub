5ec00658e299b7f46cea1570ac288eea7f9b9ab1
Eduward Post <38214928+eduwardpost@users.noreply.github.com>
2024-01-04 12:59:41 +0100

    :bookmark: Update version

--- uActivityPub/uActivityPub.cs
+++ uActivityPub/uActivityPub.cs
@@ -1,10 +1,10 @@
-using System.Diagnostics;
 using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Manifest;
 
 namespace uActivityPub;
 
+// ReSharper disable once InconsistentNaming
 public static class uActivityPub
 {
     public static string PackageName => "uActivityPub";
@@ -14,37 +14,38 @@ public class StaticAssetsBoot : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
     {
-        builder.AdduActivityPubAssets();
+        builder.AddUActivityPubAssets();
     }
 }
 
-public static class uSyncStaticAssetsExtensions
+public static class USyncStaticAssetsExtensions
 {
-    public static IUmbracoBuilder AdduActivityPubAssets(this IUmbracoBuilder builder)
+    // ReSharper disable once UnusedMethodReturnValue.Global
+    public static IUmbracoBuilder AddUActivityPubAssets(this IUmbracoBuilder builder)
     {
         // don't add if the filter is already there .
-        if (builder.ManifestFilters().Has<uActivityPubAssetManifestFilter>())
+        if (builder.ManifestFilters().Has<UActivityPubAssetManifestFilter>())
             return builder;
 
         // add the package manifest programatically. 
-        builder.ManifestFilters().Append<uActivityPubAssetManifestFilter>();
+        builder.ManifestFilters().Append<UActivityPubAssetManifestFilter>();
 
         return builder;
     }
     
-    internal class uActivityPubAssetManifestFilter : IManifestFilter
+    
+    // ReSharper disable once MemberCanBePrivate.Global
+    internal class UActivityPubAssetManifestFilter : IManifestFilter
     {
         public void Filter(List<PackageManifest> manifests)
         {
-            var assembly = typeof(uActivityPubAssetManifestFilter).Assembly;
-            FileVersionInfo fileVersionInfo = FileVersionInfo.GetVersionInfo(assembly.Location);
-            //string version = fileVersionInfo.ProductVersion;
+            var assembly = typeof(UActivityPubAssetManifestFilter).Assembly;
             
             manifests.Add(new PackageManifest
             {
-                //PackageId = "uActivityPub",
+                PackageId = "uActivityPub",
                 PackageName = uActivityPub.PackageName,
-                Version = assembly.GetName().Version.ToString(3),
+                Version = assembly.GetName().Version!.ToString(3),
                 AllowPackageTelemetry = true,
                 BundleOptions = BundleOptions.None,
                 // Scripts = new[]
