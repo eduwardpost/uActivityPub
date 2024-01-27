52f977669a9a526e0e7a5b10c84c8eafe3332eff
Eduward Post <38214928+eduwardpost@users.noreply.github.com>
2024-01-27 13:50:46 +0100

    Made webfinger controller work with single user mode and added unit tests

---
+++ src/uActivityPub.Tests/ControllerTests/WebfingerControllerTests.cs
@@ -0,0 +1,227 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using FluentAssertions;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Primitives;
+using uActivityPub.Controllers;
+using uActivityPub.Data;
+using uActivityPub.Helpers;
+using uActivityPub.Models;
+using uActivityPub.Services;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Models.Membership;
+using Umbraco.Cms.Core.Services;
+
+namespace uActivityPub.Tests.ControllerTests;
+
+public class WebfingerControllerTests
+{
+    private readonly WebfingerController _unitUnderTest;
+    
+    // Mocks
+    private readonly Mock<IUserService> _userServiceMock = new Mock<IUserService>();
+    private readonly Mock<IOptions<WebRoutingSettings>> _webRoutingSettingsMock = new Mock<IOptions<WebRoutingSettings>>();
+
+    private readonly Mock<IUActivitySettingsService> _uActivitySettingsServiceMock = new Mock<IUActivitySettingsService>();
+    
+    public WebfingerControllerTests()
+    {
+        const string baseApplicationUrl = "https://localhost.test/";
+        _webRoutingSettingsMock.Setup(x => x.Value).Returns(new WebRoutingSettings
+        {
+            UmbracoApplicationUrl = baseApplicationUrl
+        });
+        
+        _unitUnderTest = new WebfingerController(_userServiceMock.Object, _webRoutingSettingsMock.Object, _uActivitySettingsServiceMock.Object);
+    }
+
+    [Fact]
+    public void HandleRequest_Returns_Bad_Request_When_No_RequestQueryString_Is_Provided()
+    {
+        // Arrange
+        _unitUnderTest.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+        _unitUnderTest.ControllerContext.HttpContext.Request.Query = new QueryCollection(new Dictionary<string, StringValues>());
+        
+        // Act
+        var response = _unitUnderTest.HandleRequest();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Fact]
+    public void HandleRequest_Returns_Bad_Request_When_Not_Usable_RequestQueryString_Is_Provided()
+    {
+        // Arrange
+        _unitUnderTest.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+        var collectionStore = new Dictionary<string, StringValues> { { "resource", "" } };
+        _unitUnderTest.ControllerContext.HttpContext.Request.Query = new QueryCollection(collectionStore);
+        
+        // Act
+        var response = _unitUnderTest.HandleRequest();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Fact]
+    public void HandleRequest_Returns_Bad_Request_When_Not_Usable_Resource_Is_Requested()
+    {
+        // Arrange
+        _unitUnderTest.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+        var collectionStore = new Dictionary<string, StringValues> { { "resource", "unit:test" } };
+        _unitUnderTest.ControllerContext.HttpContext.Request.Query = new QueryCollection(collectionStore);
+        
+        // Act
+        var response = _unitUnderTest.HandleRequest();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Result.Should().BeOfType<BadRequestObjectResult>();
+    }
+    
+    [Fact]
+    public void HandleRequest_Returns_Webfinger_Response_When_Account_Is_Requested_In_Single_User_Mode()
+    {
+        // Arrange
+        _unitUnderTest.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+        var collectionStore = new Dictionary<string, StringValues> { { "resource", "acct:uActivityPub@umbracoSite.domain" } };
+        _unitUnderTest.ControllerContext.HttpContext.Request.Query = new QueryCollection(collectionStore);
+
+        _uActivitySettingsServiceMock.Setup(x => x.GetSettings(uActivitySettingKeys.SingleUserMode))
+            .Returns(new uActivitySettings
+            {
+                Id = 1,
+                Key = uActivitySettingKeys.SingleUserMode,
+                Value = "true"
+            });
+        
+        _uActivitySettingsServiceMock.Setup(x => x.GetSettings(uActivitySettingKeys.SingleUserModeUserName))
+            .Returns(new uActivitySettings
+            {
+                Id = 2,
+                Key = uActivitySettingKeys.SingleUserModeUserName,
+                Value = "uActivityPub"
+            });
+        
+        // Act
+        var response = _unitUnderTest.HandleRequest();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Should().BeOfType<ActionResult<WebFingerResponse>>();
+        response.Result.Should().BeOfType<OkObjectResult>();
+
+        var objectResponse = response.Result as OkObjectResult;
+        objectResponse.Should().NotBeNull();
+        objectResponse!.Value.Should().NotBeNull();
+        var webFingerResponse = objectResponse!.Value as WebFingerResponse;
+        webFingerResponse.Should().NotBeNull();
+        webFingerResponse!.Subject.Should().Be("acct:uActivityPub@umbracoSite.domain");
+        webFingerResponse.Links.Length.Should().Be(1);
+        webFingerResponse.Links.First().Rel.Should().Be("self");
+        webFingerResponse.Links.First().Type.Should().Be("application/activity+json");
+        webFingerResponse.Links.First().Href.Should().Be("https://localhost.test/activitypub/actor/uactivitypub");
+    }
+    
+    [Fact]
+    public void HandleRequest_Returns_Webfinger_Response_When_Account_Is_Requested_Not_In_Single_User_Mode()
+    {
+        // Arrange
+        _unitUnderTest.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+        var collectionStore = new Dictionary<string, StringValues> { { "resource", "acct:uActivityPub@umbracoSite.domain" } };
+        _unitUnderTest.ControllerContext.HttpContext.Request.Query = new QueryCollection(collectionStore);
+
+        _uActivitySettingsServiceMock.Setup(x => x.GetSettings(uActivitySettingKeys.SingleUserMode))
+            .Returns(new uActivitySettings
+            {
+                Id = 1,
+                Key = uActivitySettingKeys.SingleUserMode,
+                Value = "false"
+            });
+
+        long outTotal;
+        _userServiceMock.Setup(x => x.GetAll(0, 100, out outTotal))
+            .Returns(new List<IUser>
+            {
+                new User(new GlobalSettings())
+                {
+                    Name = "uActivityPub"
+                }
+            });
+        
+        // Act
+        var response = _unitUnderTest.HandleRequest();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Should().BeOfType<ActionResult<WebFingerResponse>>();
+        response.Result.Should().BeOfType<OkObjectResult>();
+
+        var objectResponse = response.Result as OkObjectResult;
+        objectResponse.Should().NotBeNull();
+        objectResponse!.Value.Should().NotBeNull();
+        var webFingerResponse = objectResponse!.Value as WebFingerResponse;
+        webFingerResponse.Should().NotBeNull();
+        webFingerResponse!.Subject.Should().Be("acct:uActivityPub@umbracoSite.domain");
+        webFingerResponse.Links.Length.Should().Be(1);
+        webFingerResponse.Links.First().Rel.Should().Be("self");
+        webFingerResponse.Links.First().Type.Should().Be("application/activity+json");
+        webFingerResponse.Links.First().Href.Should().Be("https://localhost.test/activitypub/actor/uactivitypub");
+    }
+    
+    [Fact]
+    public void HandleRequest_Returns_NotFound_Response_When_Non_Existing_Account_Is_Requested_Not_In_Single_User_Mode()
+    {
+        // Arrange
+        _unitUnderTest.ControllerContext = new ControllerContext
+        {
+            HttpContext = new DefaultHttpContext()
+        };
+        var collectionStore = new Dictionary<string, StringValues> { { "resource", "acct:uActivityPub@umbracoSite.domain" } };
+        _unitUnderTest.ControllerContext.HttpContext.Request.Query = new QueryCollection(collectionStore);
+
+        _uActivitySettingsServiceMock.Setup(x => x.GetSettings(uActivitySettingKeys.SingleUserMode))
+            .Returns(new uActivitySettings
+            {
+                Id = 1,
+                Key = uActivitySettingKeys.SingleUserMode,
+                Value = "false"
+            });
+
+        long outTotal;
+        _userServiceMock.Setup(x => x.GetAll(0, 100, out outTotal))
+            .Returns(new List<IUser>
+            {
+                Capacity = 0
+            });
+        
+        // Act
+        var response = _unitUnderTest.HandleRequest();
+
+        // Assert
+        response.Should().NotBeNull();
+        response.Should().BeOfType<ActionResult<WebFingerResponse>>();
+        response.Result.Should().BeOfType<NotFoundResult>();
+    }
+}
--- src/uActivityPub/Controllers/WebfingerController.cs
+++ src/uActivityPub/Controllers/WebfingerController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using uActivityPub.Helpers;
 using uActivityPub.Models;
+using uActivityPub.Services;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Web.Common.Controllers;
@@ -9,24 +10,18 @@ using Umbraco.Cms.Web.Common.Controllers;
 namespace uActivityPub.Controllers;
 
 [Route("/.well-known/webfinger")]
-public class WebfingerController : UmbracoApiController
+public class WebfingerController(
+    IUserService userService,
+    IOptions<WebRoutingSettings> webRoutingSettings,
+    IUActivitySettingsService uActivitySettingsService)
+    : UmbracoApiController
 {
-    private readonly IUserService _userService;
-    private readonly IOptions<WebRoutingSettings> _webRoutingSettings;
-
-    public WebfingerController(IUserService userService, IOptions<WebRoutingSettings> webRoutingSettings)
-    {
-        _userService = userService;
-        _webRoutingSettings = webRoutingSettings;
-    }
-
-
     [HttpGet("")]
     public ActionResult<WebFingerResponse> HandleRequest()
     {
         var resource = Request.Query["resource"];
 
-        if (!resource.Any())
+        if (resource.Count == 0)
             return BadRequest("Can't search for something without resource request");
 
         var requestedResource = resource.First();
@@ -48,22 +43,43 @@ public class WebfingerController : UmbracoApiController
     private ActionResult<WebFingerResponse> GetAccount(string last, string requestedResource)
     {
         var userName = last.Split('@').First().ToLower();
-        var user = _userService.GetAll(0, 100, out _).FirstOrDefault(u => u.ActivityPubUserName() == userName);
 
-        if (user == null)
-            return NotFound();
+        if (uActivitySettingsService.GetSettings(uActivitySettingKeys.SingleUserMode)!.Value == "false")
+        {
+            var user = userService.GetAll(0, 100, out _).FirstOrDefault(u => u.ActivityPubUserName() == userName);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(new WebFingerResponse
+            {
+                Subject = requestedResource,
+                Links =
+                [
+                    new WebFingerLink
+                    {
+                        Rel = "self",
+                        Href =
+                            $"{webRoutingSettings.Value.UmbracoApplicationUrl}activitypub/actor/{user.ActivityPubUserName()}"
+                    }
+                ]
+            });
+        }
+
+        var singleUserModeName = uActivitySettingsService.GetSettings(uActivitySettingKeys.SingleUserModeUserName);
 
         return Ok(new WebFingerResponse
         {
             Subject = requestedResource,
-            Links = new[]
-            {
+            Links =
+            [
                 new WebFingerLink
                 {
                     Rel = "self",
-                    Href = $"{_webRoutingSettings.Value.UmbracoApplicationUrl}activitypub/actor/{user.ActivityPubUserName()}"
+                    Href =
+                        $"{webRoutingSettings.Value.UmbracoApplicationUrl}activitypub/actor/{singleUserModeName!.Value.ToLowerInvariant()}"
                 }
-            }
+            ]
         });
     }
 }
