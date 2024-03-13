faec21dd0dc1d1369be6bc39fbf68f0c74d6042b
Eduward Post <38214928+eduwardpost@users.noreply.github.com>
2024-03-13 14:25:18 +0100

    :sparkles: Added more actor properties

--- src/uActivityPub/Models/Actor.cs
+++ src/uActivityPub/Models/Actor.cs
@@ -17,6 +17,7 @@ public class Actor : ActivityPubBase
     // ReSharper disable once UnusedMember.Global
     public string Name => PreferredUsername;
 
+    public string? Url { get; set; }
     public string Inbox { get; set; } = default!;
     public string? Outbox { get; set; }
     public string? Followers { get; set; }
@@ -24,6 +25,9 @@ public class Actor : ActivityPubBase
     public DateTime? Published { get; set; } = new DateTime(2023, 07, 17, 12, 00, 00);
     public DateTime? Updated { get; set; } = DateTime.Now;
     public bool ManuallyApprovesFollowers { get; set; } = false;
+    public bool Discoverable { get; set; } = true;
+    public bool Indexable { get; set; } = true;
+    public string Summery { get; set; } = String.Empty;
 
     public PublicKey? PublicKey { get; set; }
 
@@ -60,6 +64,8 @@ public class Actor : ActivityPubBase
 
         PreferredUsername = activityPubUserName;
         Id =
+            $"{webRoutingSettings.Value.UmbracoApplicationUrl}activitypub/actor/{activityPubUserName}";
+        Url =
             $"{webRoutingSettings.Value.UmbracoApplicationUrl}activitypub/actor/{activityPubUserName}";
         Inbox =
             $"{webRoutingSettings.Value.UmbracoApplicationUrl}activitypub/inbox/{activityPubUserName}";
