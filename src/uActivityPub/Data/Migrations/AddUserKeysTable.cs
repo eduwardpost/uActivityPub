ab2b5fcd879a3963f5fdda51dc09824bdc954a32
Eduward Post <38214928+eduwardpost@users.noreply.github.com>
2024-03-29 19:36:47 +0100

    :white_check_mark: Changed code coverage type
    Signed-off-by: Eduward Post <38214928+eduwardpost@users.noreply.github.com>

--- src/uActivityPub/Data/Migrations/AddUserKeysTable.cs
+++ src/uActivityPub/Data/Migrations/AddUserKeysTable.cs
@@ -1,8 +1,10 @@
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Infrastructure.Migrations;
 
 namespace uActivityPub.Data.Migrations;
 
+[ExcludeFromCodeCoverage]
 public class AddUserKeysTable : MigrationBase
 {
     public AddUserKeysTable(IMigrationContext context) : base(context)
@@ -13,7 +15,7 @@ public class AddUserKeysTable : MigrationBase
         Logger.LogDebug("Running migration {MigrationStep}", "AddUserKeysTable");
 
         // Lots of methods available in the MigrationBase class - discover with this.
-        if (TableExists("userKeys") == false)
+        if (TableExists("userKeys"))
         {
             Create.Table<UserKeysSchema>().Do();
         }
